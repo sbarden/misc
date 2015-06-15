1005d244200bc9404ec51727d08685df743ac449
sbarden <sbarden@inadco.com>
2015-06-15 15:46:00 -0500

    misc changes

--- src/app.php
+++ src/app.php
@@ -6,7 +6,7 @@ require __DIR__ . '/../db-connect.php';
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
-
+$app['debug'] = true
 // Setup the application
 $app = new Application();
 $app->register(new TwigServiceProvider, array(
@@ -20,11 +20,12 @@ $app['db'] = $app->share(function ($app) {
     return new PDO($app['db.dsn'], DB_USER, DB_PASSWORD);
 });
 
-//styles 
-$style_overrides = '<style>.hero-unit {background-color: green;}</style>'
+
+
 
 // Handle the index page
 $app->match('/', function () use ($app) {
+	$style_overrides = '<style>.hero-unit {background-color: green;}</style>'
     $query = $app['db']->prepare("SELECT message FROM {$app['db.table']}");
     $thoughts = $query->execute() ? $query->fetchAll(PDO::FETCH_ASSOC) : array();
 
