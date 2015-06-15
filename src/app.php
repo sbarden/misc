7631c62e4c0c33b529a006a064acf76f08203bc9
sbarden <sbarden@inadco.com>
2015-06-15 16:00:40 -0500

    select fix

--- src/app.php
+++ src/app.php
@@ -3,15 +3,23 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../db-connect.php';
 
+
 use Silex\Application;
 use Silex\Provider\TwigServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
-$app['debug'] = true
+
+
+
 // Setup the application
 $app = new Application();
+
+$app['debug'] = false;
+
+
 $app->register(new TwigServiceProvider, array(
     'twig.path' => __DIR__ . '/templates',
 ));
+
 
 // Setup the database
 $app['db.table'] = DB_TABLE;
@@ -20,20 +28,15 @@ $app['db'] = $app->share(function ($app) {
     return new PDO($app['db.dsn'], DB_USER, DB_PASSWORD);
 });
 
-
-
-
 // Handle the index page
 $app->match('/', function () use ($app) {
-	$style_overrides = '<style>.hero-unit {background-color: green;}</style>'
+    $style =  '<style>.hero-unit {background-color: green;}</style>';
     $query = $app['db']->prepare("SELECT message FROM {$app['db.table']}");
     $thoughts = $query->execute() ? $query->fetchAll(PDO::FETCH_ASSOC) : array();
-
     return $app['twig']->render('index.twig', array(
-        'title'    => 'Would you like to play a game?',
+        'title'    => 'What would you like to play?',
         'thoughts' => $thoughts,
-		'style_overrides' => $style_overrides,
+        'style'    => $style,
+
     ));
 });
-
-$app->run();
